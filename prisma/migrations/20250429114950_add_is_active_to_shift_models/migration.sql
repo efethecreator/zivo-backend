73c2ea6f80e2075046db76a6a2a093eac5e6b960
efethecreator <efe214360@hotmail.com>
2025-05-01 03:25:37 +0300

    business model edited

--- prisma/migrations/20250429114950_add_is_active_to_shift_models/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "business_shifts" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
-
--- AlterTable
-ALTER TABLE "shift_times" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
